7b9c2be26f056bae27880ae295fa2569a2604c5f
Nick <67558540+nik-1x@users.noreply.github.com>
2023-12-27 18:31:17 +0400

    last changes

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from aiogram import Bot, Dispatcher, executor, types
-import logging
 import asyncio
+import json
+import logging
+from urllib.parse import quote
+
+from aiogram import Bot, Dispatcher, executor, types
+
 from services_api import Service
 from tools import *
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(
-    token='6766163800:AAEiePkz6fEDRXdyAEF6s5lRsev4bo6220M',
-    parse_mode=types.ParseMode.MARKDOWN
-)
+bot = Bot(token="6766163800:AAEiePkz6fEDRXdyAEF6s5lRsev4bo6220M", parse_mode=types.ParseMode.MARKDOWN)
 dp = Dispatcher(bot)
 
 services = Service()
@@ -21,11 +22,7 @@ async def config_update():
         gram_ton_balances = await services.get_balances()
         gram_ton_wallets = await services.get_wallet_config()
         gram_ton_config = await services.get_app_config()
-        config = {
-            'balances': gram_ton_balances,
-            'wallets': gram_ton_wallets['wallets'],
-            'config': gram_ton_config
-        }
+        config = {"balances": gram_ton_balances, "wallets": gram_ton_wallets["wallets"], "config": gram_ton_config}
         await asyncio.sleep(5)
 
 
@@ -33,22 +30,20 @@ async def on_startup(dispatcher: Dispatcher):
     asyncio.create_task(config_update())
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=["start"])
 async def start_app(message: types.Message):
-    if config['config']['clientbot_status'] == 'Inactive':
+    if config["config"]["clientbot_status"] == "Inactive":
         await message.reply("Service under maintenance.")
         return
 
     buttons = [
-        types.InlineKeyboardButton(text='🔁 Exchange', callback_data='ex_menu'),
-        types.InlineKeyboardButton(text='📃 Inscribe', url='https://t.me/gram20bot/app')
+        types.InlineKeyboardButton(text="🔁 Exchange", callback_data="ex_menu"),
+        types.InlineKeyboardButton(text="📃 Inscribe", url="https://t.me/gram20bot/app"),
     ]
     keyboard = types.InlineKeyboardMarkup(row_width=1).add(*buttons)
 
     welcome_text = (
-        "Welcome to Umbrella\n\n"
-        "It's the first OTC bot for Gram-20 inscriptions.\n\n"
-        "Feel free to use it."
+        "Welcome to Umbrella\n\n" "It's the first OTC bot for Gram-20 inscriptions.\n\n" "Feel free to use it."
     )
     await message.reply(welcome_text, reply_markup=keyboard)
 
@@ -57,22 +52,22 @@ async def start_app(message: types.Message):
 async def select_pair(query: types.CallbackQuery):
     await query.answer()
 
-    if config['config']['clientbot_status'] == 'Inactive':
-        await bot.send_message(chat_id=query.from_user.id, text='Service under maintenance.')
+    if config["config"]["clientbot_status"] == "Inactive":
+        await bot.send_message(chat_id=query.from_user.id, text="Service under maintenance.")
         return
 
     buttons = [
-        types.InlineKeyboardButton(text='🔁 Exchange', callback_data='ex_menu'),
-        types.InlineKeyboardButton(text='📃 Inscribe', url='https://t.me/gram20bot/app')
+        types.InlineKeyboardButton(text="🔁 Exchange", callback_data="ex_menu"),
+        types.InlineKeyboardButton(text="📃 Inscribe", url="https://t.me/gram20bot/app"),
     ]
 
     keyboard = types.InlineKeyboardMarkup(row_width=1).add(*buttons)
 
     await bot.edit_message_text(
-        text='Welcome to Umbrella\n\nIt\'s the first OTC bot for Gram-20 inscriptions.\n\nFeel free to use it.',
+        text="Welcome to Umbrella\n\nIt's the first OTC bot for Gram-20 inscriptions.\n\nFeel free to use it.",
         chat_id=query.from_user.id,
         message_id=query.message.message_id,
-        reply_markup=keyboard
+        reply_markup=keyboard,
     )
 
 
@@ -80,73 +75,72 @@ async def select_pair(query: types.CallbackQuery):
 async def select_pair(query: types.CallbackQuery):
     await query.answer()
 
-    if config['config']['clientbot_status'] == 'Inactive':
-        await bot.send_message(chat_id=query.from_user.id, text='Service under maintenance.')
+    if config["config"]["clientbot_status"] == "Inactive":
+        await bot.send_message(chat_id=query.from_user.id, text="Service under maintenance.")
         return
 
     keyboard = types.InlineKeyboardMarkup(row_width=1).add(
-        types.InlineKeyboardButton(text=f'GRAM/TON', callback_data=f'exchange:GRAM_TON'),
-        types.InlineKeyboardButton(text='⬅️ Back', callback_data='menu')
+        types.InlineKeyboardButton(text="GRAM/TON", callback_data="exchange:GRAM_TON"),
+        types.InlineKeyboardButton(text="⬅️ Back", callback_data="menu"),
     )
 
     await bot.edit_message_text(
-        text='🔁 What do you want to exchange?',
+        text="🔁 What do you want to exchange?",
         chat_id=query.from_user.id,
         message_id=query.message.message_id,
-        reply_markup=keyboard
+        reply_markup=keyboard,
     )
 
 
-@dp.callback_query_handler(lambda c: c.data.startswith('exchange'))
+@dp.callback_query_handler(lambda c: c.data.startswith("exchange"))
 async def pair_selected(data: types.CallbackQuery):
     await data.answer()
 
-    if config['config']['clientbot_status'] == 'Inactive':
-        await bot.send_message(chat_id=data.from_user.id, text='Service under maintenance.')
+    if config["config"]["clientbot_status"] == "Inactive":
+        await bot.send_message(chat_id=data.from_user.id, text="Service under maintenance.")
         return
 
-    pair_from, pair_to = data.data.replace('exchange:', '').split('_')
+    pair_from, pair_to = data.data.replace("exchange:", "").split("_")
 
     await bot.edit_message_text(
         message_id=data.message.message_id,
         chat_id=data.from_user.id,
-        text=f'🔁 Selected pair *{pair_from}/{pair_to}*\n\nWhat you gonna do?',
+        text=f"🔁 Selected pair *{pair_from}/{pair_to}*\n\nWhat you gonna do?",
         reply_markup=types.InlineKeyboardMarkup(2).add(
-            types.InlineKeyboardButton(text=f'Buy {pair_from}', callback_data='buy:' + pair_from + '_' + pair_to),
-            types.InlineKeyboardButton(text=f'Sell {pair_from}', callback_data='sell:' + pair_from + '_' + pair_to),
-            types.InlineKeyboardButton(text='⬅️ Back', callback_data='ex_menu')
-        )
+            types.InlineKeyboardButton(text=f"Buy {pair_from}", callback_data="buy:" + pair_from + "_" + pair_to),
+            types.InlineKeyboardButton(text=f"Sell {pair_from}", callback_data="sell:" + pair_from + "_" + pair_to),
+            types.InlineKeyboardButton(text="⬅️ Back", callback_data="ex_menu"),
+        ),
     )
 
 
-@dp.callback_query_handler(lambda c: c.data.startswith('buy'))
+@dp.callback_query_handler(lambda c: c.data.startswith("buy"))
 async def pair_selected(data: types.CallbackQuery):
-
     await data.answer()
 
-    if config['config']['clientbot_status'] == 'Inactive':
-        await bot.send_message(chat_id=data.from_user.id, text='Service under maintenance.')
+    if config["config"]["clientbot_status"] == "Inactive":
+        await bot.send_message(chat_id=data.from_user.id, text="Service under maintenance.")
         return
 
-    pair_from, pair_to = data.data.replace('buy:', '').split('_')
+    pair_from, pair_to = data.data.replace("buy:", "").split("_")
 
-    price = config['config']['sellprice_per_gram']  # in tons, /10000
+    price = config["config"]["sellprice_per_gram"]  # in tons, /10000
 
-    hot_grams_balance = config['balances']['gram']['send']['balance']  # in grams
+    hot_grams_balance = config["balances"]["gram"]["send"]["balance"]  # in grams
     # hot_grams_balance_ton = convert_g2t(hot_grams_balance, price)
 
-    min_by_config = config['config']['minsell_in_ton']  # in tons
+    min_by_config = config["config"]["minsell_in_ton"]  # in tons
     # min_by_config_gram = convert_t2g(min_by_config, price)  # in grams
 
-    max_by_config = config['config']['maxsell_in_ton']  # in tons
+    max_by_config = config["config"]["maxsell_in_ton"]  # in tons
     max_by_config_gram = convert_t2g(max_by_config, price)  # in grams
 
-    wallet_to_send_to = config['wallets']['ton_receive_address']
+    wallet_to_send_to = config["wallets"]["ton_receive_address"]
 
     available_max = convert_g2t(min(hot_grams_balance, max_by_config_gram), price)
 
     if min_by_config > available_max:
-        await bot.send_message(chat_id=data.from_user.id, text='Pool not available')
+        await bot.send_message(chat_id=data.from_user.id, text="Pool not available")
         return
 
     buy_message = (
@@ -157,39 +151,47 @@ async def pair_selected(data: types.CallbackQuery):
         "_If you send less or more than limits, you will lose your funds._"
     )
 
+    # @wallet, tonkeeper, tonhub, mytonwallet
+
     await bot.edit_message_text(
         text=buy_message,
         chat_id=data.from_user.id,
         message_id=data.message.message_id,
-        reply_markup=types.InlineKeyboardMarkup().add(
-            types.InlineKeyboardButton(text='💸 Transfer TON', url=f'ton://transfer/{wallet_to_send_to}'),
-            types.InlineKeyboardButton(text='⬅️ Back', callback_data=f'exchange:{pair_from}_{pair_to}')
-        )
+        reply_markup=types.InlineKeyboardMarkup(1).add(
+            types.InlineKeyboardButton(text="💸 Pay with default wallet", url=f"ton://transfer/{wallet_to_send_to}"),
+            types.InlineKeyboardButton(text="💸 Pay with @Wallet",
+                                       url=f"https://t.me/wallet?startattach"),
+            types.InlineKeyboardButton(text="💸 Pay with Tonkeeper",
+                                       url=f"https://app.tonkeeper.com/transfer/{wallet_to_send_to}?amount=0"),
+            types.InlineKeyboardButton(text="💸 Pay with Tonhub",
+                                       url=f"https://tonhub.com/transfer/{wallet_to_send_to}?amount=0"),
+            types.InlineKeyboardButton(text="⬅️ Back", callback_data=f"exchange:{pair_from}_{pair_to}"),
+        ),
     )
 
 
-@dp.callback_query_handler(lambda c: c.data.startswith('sell'))
+@dp.callback_query_handler(lambda c: c.data.startswith("sell"))
 async def pair_selected(data: types.CallbackQuery):
     await data.answer()
 
-    if config['config']['clientbot_status'] == 'Inactive':
-        await bot.send_message(chat_id=data.from_user.id, text='Service under maintenance.')
+    if config["config"]["clientbot_status"] == "Inactive":
+        await bot.send_message(chat_id=data.from_user.id, text="Service under maintenance.")
         return
 
-    pair_from, pair_to = data.data.replace('sell:', '').split('_')
+    pair_from, pair_to = data.data.replace("sell:", "").split("_")
 
-    price = config['config']['buyprice_per_gram']  # in tons, /10000
+    price = config["config"]["buyprice_per_gram"]  # in tons, /10000
 
-    hot_ton_balance = config['balances']['ton']['send']['balance']  # in tons
+    hot_ton_balance = config["balances"]["ton"]["send"]["balance"]  # in tons
     hot_ton_balance_gram = convert_t2g(hot_ton_balance, price)
 
-    min_by_config = config['config']['minbuy_in_ton']  # in tons
+    min_by_config = config["config"]["minbuy_in_ton"]  # in tons
     min_by_config_gram = convert_t2g(min_by_config, price)  # in grams
 
-    max_by_config = config['config']['maxbuy_in_ton']  # in tons
+    max_by_config = config["config"]["maxbuy_in_ton"]  # in tons
     max_by_config_gram = convert_t2g(max_by_config, price)  # in grams
 
-    wallet_to_send_to = config['wallets']['gram20_receive_address']
+    wallet_to_send_to = config["wallets"]["gram20_receive_address"]
 
     available_max = min(max_by_config_gram, hot_ton_balance_gram)
 
@@ -198,7 +200,7 @@ async def pair_selected(data: types.CallbackQuery):
     # print('min_by_config_gram', min_by_config_gram)
 
     if min_by_config_gram > available_max:
-        await bot.send_message(chat_id=data.from_user.id, text='Pool not available')
+        await bot.send_message(chat_id=data.from_user.id, text="Pool not available")
         return
 
     sell_message = (
@@ -209,15 +211,38 @@ async def pair_selected(data: types.CallbackQuery):
         "_If you send less or more than limits, you will lose your funds._"
     )
 
+    to_user_send = 10000 if int(available_max) > 10000 else int(available_max)
+
     await bot.edit_message_text(
         text=sell_message,
         chat_id=data.from_user.id,
         message_id=data.message.message_id,
         reply_markup=types.InlineKeyboardMarkup().add(
-            types.InlineKeyboardButton(text='💸 Transfer GRAM', url=f'https://t.me/gram20bot/app'),
-            types.InlineKeyboardButton(text='⬅️ Back', callback_data=f'exchange:{pair_from}_{pair_to}')
-        )
+            types.InlineKeyboardButton(
+                text="💸 Transfer GRAM",
+                url="https://t.me/gram20stagingbot/app?startapp="
+                + quote(
+                    json.dumps(
+                        {
+                            "to": wallet_to_send_to,
+                            "amount": to_user_send,
+                            "tick": "gram",
+                            "memo": "exchange",
+                            "type": "transfer",
+                        }
+                    )
+                )
+                .replace("%", "--")
+                .replace(
+                    "=",
+                    "__",
+                )
+                .replace("&", "-"),
+            ),
+            types.InlineKeyboardButton(text="⬅️ Back", callback_data=f"exchange:{pair_from}_{pair_to}"),
+        ),
     )
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
